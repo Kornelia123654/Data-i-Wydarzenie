ddb863cb4a1e3b99a4897ac6a62ad22f17015a22
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-02-04 12:12:41 +0100

    Dodanie treści zadania

---
+++ src/main/java/TreśćZadania.java
@@ -0,0 +1,17 @@
+public class TreśćZadania {
+  /*  Utwórz własny typ daty np. MojaData,
+    który przyjmuje 3 argumenty typu int dla określenia dni, miesięcy i lat.
+    Dodaj metodę wyświetl datę, który wyświetli datę w następujący sposób:
+    a. 1.11.2011 (pierwszy listopada 2011)
+    b. 01.11.2011
+    c. * 1 lis 2011
+
+
+Rozwinięcie zadania Daty  – Utwórz klasę Wydarzenie o 2 polach – data wydarzenia (MojaData) oraz nazwa wydarzenia. Utwórz metodę:
+a. ileZostaloLat:int – zwraca czas w latach
+b. ileZostaloMiesiecy:int – zwraca czas w miesiacach
+c. ileZostaloDni:int – zwraca czas w dniach
+
+
+   */
+}
